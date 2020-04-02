128ca5f3903fa8a9849a6dd26a30f15d2038a766
Rodolfo-Rodrigues <50324906+Rodolfo-Rodrigues@users.noreply.github.com>
2020-04-02 11:10:18 -0300

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,5 +1,14 @@
-var menu = document.querySelector('.menu ul');
+let showMenu = document.querySelector('.show_menu').addEventListener('click', clicar)
+function clicar(){
+	let menu = document.querySelector('.menu ul')
+	menu.classList.toggle('active')
+}
+
+
+
+/*var menu = document.querySelector('.menu ul');
 var showMenu = document.querySelector('.show_menu');
 showMenu.addEventListener('click', function () {
     menu.classList.toggle('active');
 })
+*/
