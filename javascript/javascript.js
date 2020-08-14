03e57adf1e13bd085b69368327906be4532a7e35
Rodolfo-Rodrigues <50324906+Rodolfo-Rodrigues@users.noreply.github.com>
2020-08-13 21:00:13 -0300

    Update javascript.js

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,3 +2,7 @@ $('.carousel').carousel({
     interval: 8000, //muda de um slide para outro a cada 18 segundos
     pause: false //nao pausa o ciclo do slider quando o cursor do mouse está em cima do slider
 })
+
+$('.navbar-nav>li>a').on('click', function(){ //código para quando o usuário clicar em um link do menu, a ul fechar.
+    $('.navbar-collapse').collapse('hide');
+});
